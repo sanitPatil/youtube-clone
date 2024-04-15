4c39c033f6c42c9325bdee67eca09e28e7aa9b83
sanitPatil <raghavpatilrp5@gmail.com>
2024-04-15 19:56:17 +0530

    user and video model added

---
+++ src/models/user.model.js
@@ -0,0 +1,92 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const userSchema = new Schema({
+    userName:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true  // OPTIMIZE SEARCHING BUT SOME OVERHEAD
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    fullName:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String, // CLOUDINARY URI
+        required:true
+    },
+    coverImage:{
+        type:true,
+    },
+    password:{
+        type:String,
+        required:[true,"password is required!"],
+    },
+    refreshToken:{
+        type:String,
+    },
+    watchHistory:[
+        {
+        type:Schema.Types.ObjectId,
+        ref:Video
+        }
+    ]
+},{timestamps:true});
+
+userSchema.pre("save", async function(next){ // middleware
+    if(!this.isModified("password")) next(); // only when password field is changed
+    
+    this.password =await bcrypt.hash(this.password,10) // field and rounds 
+    next();
+})
+
+// USER METHODS 
+userSchema.methods.isPassowrdCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){ // NO NEED FOR ASYNC AS IT FASTER WORK
+    jwt.sign({
+        _id:this._id,
+        userName:this.userName,
+        email:this.email,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+
+userSchema.methods.generateRefreshToken = function(){ // NO NEED FOR ASYNC AS IT FASTER WORK
+    jwt.sign({ 
+        _id:this._id,  // THIS IS PAYLOAD - PAYLOAD NOTHING BUT DATA
+    },
+    process.env.REFRESH_TOKEN_SECRET, // REFRESH TOKEN SECRET 
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY // EXPIRY
+    }
+)
+}
+
+
+
+export const User = mongoose.model("User",userSchema)
+
