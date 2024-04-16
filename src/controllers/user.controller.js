d5a2832d480be908e29d8a09513ed90a51842fbd
sanitPatil <raghavpatilrp5@gmail.com>
2024-04-16 17:38:39 +0530

    done some minor changes in user.controller.js module

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -83,7 +83,7 @@ const loginUser = asyncHandler(async (req,res)=>{
 
     const {email,userName,password} = req.body;
 
-    if(!userName || !email){
+    if(!(userName && email)){ // to choose any one of them  !(userName && email)
         throw new ApiError(400,"username and email is required!!!");
     }
 
